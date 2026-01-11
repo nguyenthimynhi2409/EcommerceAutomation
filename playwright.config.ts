a042576a02af84360c95a6bd7e5b6ae9fa98e59a
nguyenthimynhi2409 <nguyenthimynhi2409@gmail.com>
2026-01-11 09:49:57 +0700

    Set headles

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    headless: false,        // open browser
+    //headless: false,        // open browser
   },
 
   /* Configure projects for major browsers */
